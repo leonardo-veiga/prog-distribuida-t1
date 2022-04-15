ec08ea769cd3a0553b7dcfa3a2cdb26f02471a45
Joao Bitelo <jfbitelo@hotmail.com>
2022-04-15 14:32:26 -0300

    cria logica (isolada) de recebimento e envio de arquivos

---
+++ src/FileApi/Manipulation/FileLumper.java
@@ -0,0 +1,80 @@
+package FileApi.Manipulation;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+
+public class FileLumper {
+    // Caminho relativo para o arquivo
+    String path = "src/Receiver/files/";
+    // Nome do arquivo gerado
+    String fileName = "File.pdf";
+
+    public FileLumper() {}
+
+    // Salva um arquivo recebido
+    public void saveFile(int lastReadFileNumer, byte[] fileByteArray) throws IOException {
+        // Corrige nome do arquivo
+        String lastReadFile = Integer.toString(lastReadFileNumer);
+        while (lastReadFile.length() < 3) {
+            lastReadFile = "0" + lastReadFile;
+        }
+        System.out.println("arquivo: " + lastReadFile);
+        String composedName = path + fileName + "." + lastReadFile;
+        File f = new File(composedName); // Creating the file
+        FileOutputStream outToFile = new FileOutputStream(f);
+        outToFile.write(fileByteArray);
+        outToFile.close();
+    }
+
+    public void mergeFiles() throws IOException {
+        System.out.println("mergeando arquivos");
+        String composedName = path + fileName;
+        try (FileOutputStream fos = new FileOutputStream(composedName);
+                BufferedOutputStream mergingStream = new BufferedOutputStream(fos)) {
+            for (File f : listOfFilesToMerge()) {
+                Files.copy(f.toPath(), mergingStream);
+            }
+        }
+
+    }
+
+    public List<File> listOfFilesToMerge() {
+        String composedName = path + fileName + ".001";
+        File tmpFile = new File(composedName);
+        String tmpName = tmpFile.getName();// {name}.{number}
+        String destFileName = tmpName.substring(0, tmpName.lastIndexOf('.'));// remove .{number}
+        File[] files = tmpFile.getParentFile()
+                .listFiles((File dir, String name) -> name.matches(destFileName + "[.]\\d+"));
+        Arrays.sort(files);// ensuring order 001, 002, ..., 010, ...
+        return Arrays.asList(files);
+    }
+
+    public void printMD5() throws Exception {
+        String composedName = path + fileName;
+        byte[] bytes = createMd5(composedName);
+        String string = Base64.getEncoder().encodeToString(bytes);
+        System.out.println(string);
+    }
+
+    public byte[] createMd5(String composedName) throws Exception {
+        MessageDigest md_1 = MessageDigest.getInstance("MD5");
+        InputStream is_1 = new FileInputStream(composedName);
+        try {
+            is_1 = new DigestInputStream(is_1, md_1);
+        } finally {
+            is_1.close();
+        }
+        byte[] digest_1 = md_1.digest();
+        return digest_1;
+    }
+}
---
+++ src/FileApi/Sender.java
@@ -0,0 +1,204 @@
+package FileApi;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import FileApi.Manipulation.FileSplitter;
+import FileApi.Socket.NetPackage;
+import FileApi.Socket.Serializer;
+
+public class Sender {
+	// Nome do arquivo a ser lido.
+	// OBS: O arquivo deve estar na mesma pasta /files
+	static String fileName = "enunciado.pdf";
+	// Configs do socket
+	static int timeoutSeconds = 4;
+	static int receiverPort = 9876;
+	static int senderPort = 9878;
+	static DatagramSocket senderSocket;
+	static DatagramPacket receivedPacket;
+	static InetAddress IPAddress;
+	// Mensagens a serem lida em uma itera��o
+	static ArrayList<NetPackage> messages;
+	// Manipulador de arquivos
+	static FileSplitter fs;
+	static int fileChunks = 0;
+	static Serializer ser = new Serializer();
+	// Slow start
+	static int SlowStart = 1;
+	// Qual o ultimo arquivo enviado
+	static int seq = 0;
+	// ACK
+	static int ACK = 100;
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando...");
+		// -------------------------- //
+		System.out.print("Quebrando o arquivo ");
+		System.out.print(fileName);
+		System.out.print(" em peda�os");
+		System.out.println("");
+		fs = new FileSplitter(fileName);
+		try {
+			fileChunks = fs.split() - 1;
+			System.out.println("Arquivo quebrado em " + fileChunks + " com sucesso");
+		} catch (IOException e) {
+			System.out.println("Ocorreu um erro ao manipular o arquivo");
+		}
+		// -------------------------- //
+		System.out.println("Realizando configura��es");
+		// Configura socket para o sender
+		setSenderSocket();
+		// Declara o pacote a ser recebido
+		declarePackage();
+		// Seta ip do receiver
+		setIp();
+		System.out.println("Pronto para enviar");
+		// -------------------------- //
+
+		// Fluxo de envio de mensagens e confirma��es de entrega
+		boolean okToContinue = true;
+		while (seq < fileChunks) {
+			okToContinue = true;
+			System.out.println("/////////////////////");
+			System.out.println("seq: " + seq);
+			System.out.println("SlowStart: " + SlowStart);
+			System.out.println("ACK: " + ACK);
+			// Define o array de mensagens a enviar
+			getMessage();
+			// Envia as mensagens, N vezes, de acordo com slow start
+			for (int i = 0; i < SlowStart; i++) {
+				sendMessage(i);
+			}
+			// Recebe uma confirma��o
+			okToContinue = receiveACK();
+
+			if (okToContinue && SlowStart < 10) {
+				SlowStart = SlowStart * 2;
+			}
+		}
+		sendFinalMessage();
+		senderSocket.close();
+		try {
+			fs.printMD5();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// Recebimento de ACK
+	// Confirma��o do cliente de que recebeu corretamente os arquivos
+	private static boolean receiveACK() {
+		try {
+			senderSocket.receive(receivedPacket);
+			String receivedACK = new String(receivedPacket.getData());
+			receivedACK = receivedACK.replaceAll("[^\\d.]", "");
+			int newACK = Integer.parseInt(receivedACK);
+			System.out.println("ACK recebida: " + newACK);
+			if (newACK != (100 + SlowStart)) {
+				System.out.println("--------------");
+				System.out.println("ACK diferente do esperado:");
+				System.out.println("esperava: " + (100 + SlowStart));
+				throw new Exception();
+			}
+			ACK = newACK;
+			return true;
+		} catch (Exception e) {
+			System.out.println("--------------");
+			System.out.println("Erro ao receber ACK");
+			recoverFromError();
+			return false;
+		}
+	}
+
+	// Logica de recupera��o em caso de erro
+	private static void recoverFromError() {
+		System.out.println("seq:" + seq);
+		System.out.println("SlowStart: " + SlowStart);
+		System.out.println("ACK: " + ACK);
+		seq = seq - SlowStart;
+		SlowStart = 1;
+		ACK = 101;
+		System.out.println("--------------");
+	}
+
+	// Envio de mensagens
+	// Envia N peda�os do arquivo, de acordo com slow start
+	private static void sendMessage(int index) {
+		try {
+			if (index < messages.size()) {
+				byte[] sendData = ser.serialize(messages.get(index));
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+				senderSocket.send(sendPacket);
+			}
+
+		} catch (
+
+		IOException e1) {
+			System.out.println("Ocorreu um erro ao enviar a mensagem");
+			e1.printStackTrace();
+		}
+	}
+
+	// Logica de obten��o dos peda�os do arquivo a serem enviados
+	private static void getMessage() {
+		messages = new ArrayList<NetPackage>();
+		for (int i = 0; i < SlowStart; i++) {
+			seq++;
+			if (seq > fileChunks) {
+				break;
+			}
+			NetPackage np = new NetPackage();
+			np.createObj(ACK, seq, fs.getFile(seq));
+			messages.add(np);
+		}
+
+	}
+
+	// Config de IP
+	private static void setIp() {
+		try {
+			IPAddress = InetAddress.getByName("localhost");
+			System.out.println("Conectado ao receiver com sucesso");
+		} catch (UnknownHostException e) {
+			System.out.println("erro durante obtencao do ip do receiver");
+			e.printStackTrace();
+		}
+	}
+
+	// Config de pacote
+	private static void declarePackage() {
+		byte[] receiveData = new byte[1024];
+		receivedPacket = new DatagramPacket(receiveData, receiveData.length);
+	}
+
+	// Config do socket
+	private static void setSenderSocket() {
+		try {
+			senderSocket = new DatagramSocket(senderPort);
+			senderSocket.setSoTimeout(timeoutSeconds * 1000);
+		} catch (SocketException e) {
+			System.out.println("Erro! porta ja em uso");
+		}
+	}
+
+	// Envio da mensagem de condi��o de parada
+	private static void sendFinalMessage() {
+		byte[] sendData = new byte[1024];
+		String message = "DONE";
+		sendData = message.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receiverPort);
+		try {
+			senderSocket.send(sendPacket);
+		} catch (IOException e) {
+			System.out.println("erro durante envio do pacote ao receiver");
+			e.printStackTrace();
+		}
+	}
+}
